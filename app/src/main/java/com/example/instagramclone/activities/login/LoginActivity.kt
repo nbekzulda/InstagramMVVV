904e95177e934256d4b9967c47938995fff849e8
Nursultan <nbekzulda@gmail.com>
2020-02-10 13:51:30 +0600

    repository

--- app/src/main/java/com/example/instagramclone/activities/login/LoginActivity.kt
+++ app/src/main/java/com/example/instagramclone/activities/login/LoginActivity.kt
@@ -6,12 +6,14 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.instagramclone.R
 import com.example.instagramclone.activities.*
 import com.example.instagramclone.activities.createUser.RegisterActivity
 import com.example.instagramclone.activities.home.MainActivity
+import com.example.instagramclone.data.LoginRepositoryImpl
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
@@ -19,20 +21,50 @@ import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventList
 
 class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener,
     View.OnClickListener {
-
-    private val TAG = "LoginActivity"
-    private lateinit var mAuth: FirebaseAuth
-    private val viewModel by lazy {
-        ViewModelProviders.of(this).get(LoginViewModel::class.java)
-    }
-    private lateinit var buttonLogin: Button
-
     override fun onClick(p0: View) {
         when(p0.id){
             R.id.create_account_text -> {
                 startActivity(Intent(this, RegisterActivity::class.java))
+                finish()
             }
         }
+
+    }
+
+
+
+    private val TAG = "LoginActivity"
+    private lateinit var mAuth: FirebaseAuth
+    private val viewModel by lazy {
+        val loginRepository = LoginRepositoryImpl(firebaseAuth = FirebaseAuth.getInstance())
+        ViewModelProviders.of(this, LoginViewModelFactory(loginRepository = loginRepository)).get(LoginViewModel::class.java)
+    }
+
+    private lateinit var buttonLogin: Button
+    private lateinit var  email: TextView
+    private lateinit var password: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        Log.d(TAG, "onCreate")
+
+        KeyboardVisibilityEvent.setEventListener(this, this)
+        coordinateBtnAndInputs(
+            login_button,
+            email_Input,
+            password_Input
+        )
+        login_button.setOnClickListener(this)
+        create_account_text.setOnClickListener(this)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        bindView()
+        inputData()
+
 
     }
 
@@ -48,39 +80,25 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener,
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        bindView()
-        Log.d(TAG, "onCreate")
-
-        KeyboardVisibilityEvent.setEventListener(this, this)
-        coordinateBtnAndInputs(
-            login_button,
-            email_Input,
-            password_Input
-        )
-        login_button.setOnClickListener(this)
-        create_account_text.setOnClickListener(this)
-
-        setData()
-    }
-
     private fun bindView() {
         buttonLogin = findViewById(R.id.login_button)
+        email = findViewById(R.id.email_Input)
+        password = findViewById(R.id.password_Input)
 
-        mAuth = FirebaseAuth.getInstance()
 
         buttonLogin.setOnClickListener {
             viewModel.login(
-                email = email_Input.text.toString(),
-                password = password_Input.text.toString()
+                email = email.text.toString(),
+                password = password.text.toString()
             )
         }
+
+       Log.d("sa", "hhhhh")
+
     }
 
-    private fun setData(){
-        viewModel.liveData.observe(this, Observer { state ->
+    private fun inputData(){
+        viewModel?.liveData.observe(this, Observer { state ->
             when (state){
                 is LoginViewModel.State.ShowLoading -> {
                     progressBar.visibility = View.VISIBLE
@@ -134,6 +152,9 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener,
 //
 //
 //    }
+
+
+
 
 
 //    private fun validate(email: String, password: String) = email.isNotEmpty() && password.isNotEmpty()
--- app/src/main/java/com/example/instagramclone/activities/login/LoginViewModel.kt
+++ app/src/main/java/com/example/instagramclone/activities/login/LoginViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.instagramclone.activities.login
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,33 +9,49 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ValueEventListener
 import io.reactivex.Completable
 import io.reactivex.MaybeSource
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.functions.Function
 import org.reactivestreams.Publisher
 
-class LoginViewModel() : ViewModel() {
+class LoginViewModel(val loginRepository: LoginRepository) : ViewModel() {
 
     val liveData = MutableLiveData<State>()
+
+
 
     private fun validate(email: String, password: String) = email.isNotEmpty() && password.isNotEmpty()
 
     fun login(email: String, password: String){
         liveData.value = State.ShowLoading
-        if (validate(email, password)) {
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        liveData.value = State.Result
-                        liveData.value = State.HideLoading
-                    }
-                }
-                .addOnFailureListener {
-                    liveData.value = State.Error("Wrong email or password")
+
+        CompositeDisposable().add(
+            Observable.fromCallable { loginRepository.getlogin(email, password)}
+                .subscribe({ result ->
+                Log.d("nur_result", result.toString())
+                    liveData.value = State.Result
                     liveData.value = State.HideLoading
-                }
-        }
-        else{
-            liveData.value = State.Error("Please enter email and password")
-        }
+            })
+            )
+
+
+//        if (validate(email, password)) {
+//            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener {
+//                    if (it.isSuccessful) {
+//                        liveData.value = State.Result
+//                        liveData.value = State.HideLoading
+//                    }
+//                }
+//                .addOnFailureListener {
+//                    liveData.value = State.Error("Wrong email or password")
+//                    liveData.value = State.HideLoading
+//                }
+//        }
+//        else{
+//            liveData.value = State.Error("Please enter email and password")
+//        }
     }
 
     sealed class State{
@@ -45,8 +62,8 @@ class LoginViewModel() : ViewModel() {
     }
 
 
+}
 
-
-
+private fun CompositeDisposable.add(doOnSubscribe: Observable<Boolean>?) {
 
 }
